08045c95f4f0b08e725ef77b91f1457460234625
fersizu <79169271+fersizu@users.noreply.github.com>
2023-05-08 10:54:24 -0700

    Add files via upload

---
+++ EVA3_4_PROCEDIMIENTOS1.sql
@@ -0,0 +1,22 @@
+DELIMITER //
+create procedure listaCorreos(inout correos varchar(5000))
+begin
+	declare correo varchar (50);
+	declare terminar int default false; 
+	declare lista_clientes cursor for
+		select email from customer limit 100; 
+	declare continue handler for NOT FOUND set terminar = true; 
+  
+    
+    open lista_clientes;
+    obten_correos: loop
+		fetch lista_clientes into correo; 
+        if terminar then 
+			leave obten_correos; 
+            end if;
+        set correos = concat(correo, ";", correos);
+    end loop obten_correos;
+    close lista_clientes;
+    
+end//
+DELIMITER ; 
